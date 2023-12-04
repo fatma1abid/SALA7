c0fed455efa55ef1d61fe312e24d5246f62f1434
fatma1abid <fatma.abid@esprit.tn>
2023-12-04 21:39:32 +0100

    config

---
+++ Desktop/ESPRIT FATMAA/Integ_Web/tailwind/tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["./public/*"],  theme: {
+    extend: {
+      colors:{
+        colorblue:"#0000FF"
+      },
+      fontSize:{
+      	sizePrimary: "2rem"
+      },
+    },
+  },
+  plugins: [],
+}
+
